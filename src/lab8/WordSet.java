e7176c6d0a47f44092a760cd09325f7fc727baab
basegodaa <basegodaa@gmail.com>
2014-10-10 22:20:23 -0600

    Update WordSet.java

--- src/lab8/WordSet.java
+++ src/lab8/WordSet.java
@@ -1,6 +1,11 @@
 package Lab8;
 /*
+	Sergio Cancinos, 	13062
+	Angel Basegoda, 	13256
+	Augusto Cardenas, 	13136
 
+	Bibliografia
+http://algs4.cs.princeton.edu/33balanced/RedBlackBST.java.html
 */
 public interface WordSet {
 
